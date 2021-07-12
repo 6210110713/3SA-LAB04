a1a54734565ed478859e33337a5b256ec129fecb
6210110713 <6210110713@psu.ac.th>
2021-07-12 10:38:33 +0700

    Check05

--- components/Weather.js
+++ components/Weather.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import { Text, ImageBackground ,StyleSheet} from 'react-native';
 import Forecast from './Forecast';
 
 export default function Weather(props) {
+    const [forecastInfo, setForecastInfo] = useState({
+        main: '-',
+        description: '-',
+        temp: 0
+        })
+
     useEffect(() => {
         console.log(`fetching data with zipCode = ${props.zipCode}`)
         if (props.zipCode) {
@@ -20,11 +26,6 @@ export default function Weather(props) {
         });
         }
     }, [props.zipCode])       
-    const [forecastInfo, setForecastInfo] = useState({
-    main: '-',
-    description: '-',
-    temp: 0
-    })
     
     return (
         <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
