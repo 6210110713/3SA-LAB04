032875112ebdf0dd86030c0d19d69585ff25f8e6
6210110713 <6210110713@psu.ac.th>
2021-07-12 10:02:20 +0700

    Check03

--- App.js
+++ App.js
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Weather from './components/Weather';
+import constants from 'expo-constants';
+
+
 export default function App() {
- const doIt = () => {
- console.log("Hello from console")
- }
  return (
 <View style={styles.container}>
  <Weather zipCode="90110"/>
